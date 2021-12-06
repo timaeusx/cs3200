e08388aca5c9b1ffb7cbcefe83bb6ff7501825b0
timaeus <wto.mailbox@gmail.com>
2021-12-05 21:25:02 -0500

    Update User repository and DAO to support create, update, and delete operations

--- tt-server/src/main/java/com/example/ttserver/daos/UserDao.java
+++ tt-server/src/main/java/com/example/ttserver/daos/UserDao.java
@@ -4,21 +4,47 @@ import com.example.ttserver.models.User;
 import com.example.ttserver.repositories.UserRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class UserDao {
   @Autowired
   UserRepository userRepository;
+
+  @PostMapping("/api/users")
+  public User createUser(@RequestBody User user) {
+    return userRepository.save(user);
+  }
+
   @GetMapping("/api/users")
   public List<User> findAllUsers() {
     return (List<User>) userRepository.findAll();
   }
 
   @GetMapping("/api/users/{userId}")
-  public User findUserById(@PathVariable("userId") Integer userId) {
-    return userRepository.findById(userId).get();
+  public User findUserById(@PathVariable("userId") Integer id) {
+    return userRepository.findById(id).get();
+  }
+
+  @PutMapping("/api/users/{userId}")
+  public User updateUser(
+      @PathVariable("userId") Integer id,
+      @RequestBody User userUpdates) {
+    User user = userRepository.findUserById(id);
+    user.setFirstname(userUpdates.getFirstname());
+    user.setLastname(userUpdates.getLastname());
+    user.setUsername(userUpdates.getUsername());
+    user.setPassword(userUpdates.getPassword());
+    user.setEmail(userUpdates.getEmail());
+    user.setDob(userUpdates.getDob());
+    return userRepository.save(user);
+  }
+
+  @DeleteMapping("/api/users/{userId}")
+  public void deleteUser(
+      @PathVariable("userId") Integer id) {
+    userRepository.deleteById(id);
   }
 }
--- tt-server/src/main/java/com/example/ttserver/repositories/UserRepository.java
+++ tt-server/src/main/java/com/example/ttserver/repositories/UserRepository.java
@@ -1,8 +1,15 @@
 package com.example.ttserver.repositories;
 
 import com.example.ttserver.models.User;
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
+  @Query(value="SELECT * FROM users", nativeQuery=true)
+  public List<User> findAllUsers();
 
+  @Query(value="SELECT * FROM users WHERE id=:userId", nativeQuery=true)
+  public User findUserById(@Param("userId") Integer id);
 }
