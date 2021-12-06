360018bfefa9bf08def25a3b5f508b2de8e4a424
timaeus <wto.mailbox@gmail.com>
2021-12-05 21:33:56 -0500

    Update TransitCard repository and DAO to support create, update, and delete operations

--- tt-server/src/main/java/com/example/ttserver/daos/TransitCardDao.java
+++ tt-server/src/main/java/com/example/ttserver/daos/TransitCardDao.java
@@ -4,14 +4,18 @@ import com.example.ttserver.models.TransitCard;
 import com.example.ttserver.repositories.TransitCardRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TransitCardDao {
   @Autowired
   TransitCardRepository transitCardRepository;
+
+  @PostMapping("/api/transitcards")
+  public TransitCard createTransitCard(@RequestBody TransitCard card) {
+    return transitCardRepository.save(card);
+  }
+
   @GetMapping("/api/transitcards")
   public List<TransitCard> findAllTransitCards() {
     return (List<TransitCard>) transitCardRepository.findAll();
@@ -20,5 +24,20 @@ public class TransitCardDao {
   @GetMapping("/api/transitcards/{cardId}")
   public TransitCard findTransitCardById(@PathVariable("cardId") Integer idtransitcard) {
     return transitCardRepository.findById(idtransitcard).get();
+  }
+
+  @PutMapping("/api/transitcards/{cardId}")
+  public TransitCard updateTransitCard(@PathVariable("cardId") Integer id,
+                                       @RequestBody TransitCard cardUpdates) {
+    TransitCard card = transitCardRepository.findTransitCardById(id);
+    card.setStoredvalue(cardUpdates.getStoredvalue());
+    card.setExpiration(cardUpdates.getExpiration());
+    card.setUserid(cardUpdates.getUserid());
+    return transitCardRepository.save(card);
+  }
+
+  @DeleteMapping("/api/transitcards/{cardId}")
+  public void deleteTransitCard(@PathVariable("cardId") Integer id) {
+    transitCardRepository.deleteById(id);
   }
 }
--- tt-server/src/main/java/com/example/ttserver/repositories/TransitCardRepository.java
+++ tt-server/src/main/java/com/example/ttserver/repositories/TransitCardRepository.java
@@ -1,7 +1,16 @@
 package com.example.ttserver.repositories;
 
 import com.example.ttserver.models.TransitCard;
+import com.example.ttserver.models.User;
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface TransitCardRepository extends CrudRepository<TransitCard, Integer> {
+  @Query(value="SELECT * FROM transitcards", nativeQuery=true)
+  public List<TransitCard> findAllTransitCards();
+
+  @Query(value="SELECT * FROM transitcards WHERE idtransitcards=:idtransitcard", nativeQuery=true)
+  public TransitCard findTransitCardById(@Param("idtransitcard") Integer id);
 }
