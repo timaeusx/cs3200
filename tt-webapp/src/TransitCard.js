2650431ac5dc9cc041685336138a8c42aaae0ec8
timaeus <wto.mailbox@gmail.com>
2021-12-06 13:05:33 -0500

    Add TransitCard.js

--- tt-webapp/src/TransitCard.js
+++ tt-webapp/src/TransitCard.js
@@ -2,8 +2,15 @@ const TransitCard = ({transitCard}) => {
   return(
       <div>
         <b>
-          {transitCard.ownedBy.firstName}
+          {transitCard.ownedBy.firstname}&nbsp;{transitCard.ownedBy.lastname}'s Card
         </b>
+        <p>
+          Stored Value:&nbsp;{transitCard.storedvalue}
+        </p>
+        <p>
+          Expiration:&nbsp;{transitCard.expiration}
+        </p>
       </div>
-  )
+  );
 }
+export default TransitCard
