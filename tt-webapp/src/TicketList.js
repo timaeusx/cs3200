fa954e39973a467711cc5b37304800ef9f532b5d
timaeus <wto.mailbox@gmail.com>
2021-12-06 19:34:54 -0500

    Implement Ticket edit screen and general cleanup/prettifying

--- tt-webapp/src/TicketList.js
+++ tt-webapp/src/TicketList.js
@@ -1,24 +1,36 @@
 import {useState, useEffect} from "react";
-import {useParams} from "react-router-dom";
+import {useNavigate} from "react-router"
+import {Link, useParams} from "react-router-dom";
 
 const TicketList = () => {
-  const [tickets, setTickets] = useState([]);
+
+  const navigate = useNavigate();
   const params = useParams();
+
+  const [tickets, setTickets] = useState([]);
   const findTicketsById = () =>
       fetch(`http://localhost:8080/api/transitcards/${params.id}/tickets`)
       .then(res => res.json())
       .then(tickets => setTickets(tickets));
   useEffect(findTicketsById, [params.id])
+
   return(
     <div>
       <h1>
         Tickets
       </h1>
+      <button className = "btn btn-primary"
+              onClick = {() => navigate("/tickets/new/edit")}>
+        New Ticket...
+      </button>
       <ul>
         {
           tickets.map(ticket =>
               <li key={ticket.idtickets}>
                 Ticket ID:&nbsp;{ticket.idtickets}
+                <Link to={`/tickets/${ticket.idtickets}/edit`}>
+                  &nbsp;Edit Ticket
+                </Link>
                 <ul>
                   <li>
                     Expiration:&nbsp;{ticket.expiration}
@@ -34,7 +46,9 @@ const TicketList = () => {
           )
         }
       </ul>
+      <button onClick={() => {navigate(-1)}}>Back</button>
     </div>
   );
 };
+
 export default TicketList;
